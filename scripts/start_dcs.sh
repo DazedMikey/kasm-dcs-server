ef76408a73a98ff01cafae28507f0c244be77609
Michael Napodano <michaelpnap@gmail.com>
2024-02-20 09:23:31 -0700

    Added comments and TODO item

--- scripts/start_dcs.sh
+++ scripts/start_dcs.sh
@@ -3,13 +3,16 @@
 # Link DCS
 ln -s ~/wine/drive_c/Program\ Files/Eagle\ Dynamics/DCS\ World\ OpenBeta\ Server dcs_server
 
+# Give kasm-user access to wine directory
 sudo chown -R kasm-user /home/kasm-user/wine
 
-# DCS command
+# Open firefox to server command window
 firefox ~/dcs_server/WebGUI/index.html &
 
+# DCS command:
+# TODO - Change path to be generi user. Also need to update instructions.
 LIBERATION_EXPORT_DIR=/home/kasm-user/wine/drive_c/users/sly/Saved\ Games/DCS.openbeta_server \
-  WINEARCH="win64" \
+	WINEARCH="win64" \
 	WINEDLLOVERRIDES="wbemprox=n" \
 	WINEPREFIX="/home/kasm-user/wine" \
 	wine ~/dcs_server/bin/DCS_server.exe --norender
